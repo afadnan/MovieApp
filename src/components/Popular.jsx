e8c3413a774a4bc37a7544b0462ee49d96e26ddf
Adnan Ashraf <af.adnan23@gmail.com>
2024-08-20 18:46:40 +0530

    copied from Trending to Popular page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import axios from "../utils/axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
+import Cards from "./partials/Cards";``
+import Topnav from "./partials/Topnav";
+import Dropdown from "./partials/Dropdown";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Popular = () => {
-  return (
-    <div>Popular</div>
-  )
-}
+  const navigate = useNavigate();
+  const [category, setcategory] = useState("movie");
+  const [popular, setpopular] = useState([]);
+  const [page, setpage] = useState(1);
+  const [hasMore, sethasMore] = useState(true);
 
-export default Popular
+  const GetPopular = async () => {
+    try {
+      const { data } = await axios.get(`${category}/popular?page=${page}`);
+      console.log(data);
+
+      if (data.results.length > 0) {
+        setpopular((prevState) => [...prevState, ...data.results]);
+        setpage(page + 1);
+      } else {
+        sethasMore(false);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+  //console.log(popular);
+  const refreshHandler = async () => {
+    if (popular.length === 0) {
+      GetPopular();
+    } else {
+      setpage(1);
+      setpopular([]);
+      GetPopular();
+    }
+  };
+  useEffect(() => {
+    refreshHandler();
+  }, [category]);
+
+  return popular.length > 0 ? (
+    <div className="w-screen h-screen  ">
+      <div className="w-full flex items-center justify-between px-[4%] ">
+        <h1 className=" text-2xl font-bold text-zinc-400">
+          <i
+            onClick={() => navigate(-1)}
+            className=" text-[#07e1f5]  hover:text-[#27a0ab] ri-arrow-left-line"
+          ></i>
+          popular
+        </h1>
+
+        <div className="flex items-center w-[85%] mt-[3%]">
+          <Topnav />
+
+          <Dropdown
+            title="Category"
+            options={["movie", "tv", "all"]}
+            func={(e) => setcategory(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <InfiniteScroll
+        dataLength={popular.length}
+        next={GetPopular}
+        hasMore={hasMore}
+        loader={<h1>Loading...</h1>}
+      >
+        <Cards data={popular} title={category} />
+      </InfiniteScroll>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Popular;
