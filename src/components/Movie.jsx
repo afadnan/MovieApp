bfcd1b383675e08368b0c9b20f9b95b8b9e1605c
Adnan Ashraf <af.adnan23@gmail.com>
2024-08-21 15:27:08 +0530

    Completed Movie page with API of TMDB

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import axios from "../utils/axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
+import Cards from "./partials/Cards";
+import Topnav from "./partials/Topnav";
+import Dropdown from "./partials/Dropdown";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Movie = () => {
-  return (
-    <div>Movie</div>
-  )
-}
+  const navigate = useNavigate();
+  const [category, setcategory] = useState("now_playing");
+  const [movie, setmovie] = useState([]);
+  const [page, setpage] = useState(1);
+  const [hasMore, sethasMore] = useState(true);
+  document.title = "Movie App | MoviesPage ";
+  const GetMovie = async () => {
+    try {
+      const { data } = await axios.get(`/movie/${category}?page=${page}`);
+      console.log(data);
+      if (data.results.length > 0) {
+        setmovie((prevState) => [...prevState, ...data.results]);
+        setpage(page + 1);
+      } else {
+        sethasMore(false);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+  //console.log(movie);
+  const refreshHandler = async () => {
+    if (movie.length === 0) {
+      GetMovie();
+    } else {
+      setpage(1);
+      setmovie([]);
+      GetMovie();
+    }
+  };
+  useEffect(() => {
+    refreshHandler();
+  }, [category]);
 
-export default Movie
+  return movie.length > 0 ? (
+    <div className="w-screen h-screen  ">
+      <div className="w-full flex items-center justify-between px-[4%] ">
+        <h1 className=" text-2xl font-bold text-zinc-400">
+          <i
+            onClick={() => navigate(-1)}
+            className=" text-[#07e1f5]  hover:text-[#27a0ab] ri-arrow-left-line"
+          ></i>
+          Movie
+        </h1>
+        <div className="flex items-center w-[85%] mt-[3%]">
+          <Topnav />
+          <Dropdown
+            title="Category"
+            options={["popular", "top_rated", "upcoming", "now_playing"]}
+            func={(e) => setcategory(e.target.value)}
+          />
+        </div>
+      </div>
+      <InfiniteScroll
+        dataLength={movie.length}
+        next={GetMovie}
+        hasMore={hasMore}
+        loader={<h1>Loading...</h1>}
+      >
+        <Cards data={movie} title={category} />
+      </InfiniteScroll>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Movie;
