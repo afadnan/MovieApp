22348879cbcb4870802a4d7fcda0735ad479a4d3
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-01 19:33:06 +0530

    getting MovieDetails and removing MD in Redux

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { asyncloadmovie } from "../store/actions/movieActions";
+import { asyncloadmovie, removemovie } from "../store/actions/movieActions";
 import { useParams } from "react-router-dom";
 const Moviedetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(asyncloadmovie(id));
+    return () => {
+      dispatch(removemovie());
+    };
   }, []);
   return <div>Moviedetails</div>;
 };
--- src/store/actions/movieActions.jsx
+++ src/store/actions/movieActions.jsx
@@ -13,11 +13,12 @@ export const asyncloadmovie = (id) => async (dispatch, getState) => {
     let theultimatedetais = {
       detail: detail.data,
       externalid: externalid.data,
-      recommendations: recommendations.data,
-      similar: similar.data,
-      videos: videos.data,
-      watchproviders: watchproviders.data,
+      recommendations: recommendations.data.results,
+      similar: similar.data.results,
+      videos: videos.data.results.find((m) => m.type === "Trailer"),
+      watchproviders: watchproviders.data.results.IN,
     };
+    dispatch(loadmovie(theultimatedetais));
     console.log(theultimatedetais);
   } catch (error) {
     console.log("Error: ", error);
