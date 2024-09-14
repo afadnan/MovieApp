f7c797cabf73dd097755ded0c3668e5951f32f95
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-15 00:37:53 +0530

    Close icon added

--- src/components/partials/Trailer.jsx
+++ src/components/partials/Trailer.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Trailer = () => {
+  const navigate = useNavigate();
   const { pathname } = useLocation();
-
   const category = pathname.includes("movie") ? "movie" : "tv";
   const ytvideo = useSelector((state) => state[category].info.videos);
   console.log(ytvideo);
 
   return (
-    <div className="bg-[rgba(0,0,0,.7)] absolute z-[100] bg-black top-0 left-0 w-screen h-screen flex items-center justify-center">
-      <ReactPlayer url={`https://www.youtube.com/watch?v=${ytvideo.key}`} />
+    <div className="bg-[rgba(0,0,0,.9)] absolute z-[100] bg-black top-0 left-0 w-screen h-screen flex items-center justify-center">
+      <Link
+        onClick={() => navigate(-1)}
+        className="absolute hover:text-[#6556CD] ri-close-fill text-4xl text-white right-[5%] top-[5%]"
+      ></Link>
+      <ReactPlayer
+        height={650}
+        width={1100}
+        url={`https://www.youtube.com/watch?v=${ytvideo.key}`}
+      />
     </div>
   );
 };
