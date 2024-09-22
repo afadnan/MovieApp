2e87ae7266792dfa9945856922d6aaa74765b1cf
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-22 12:42:01 +0530

    Inside Cards NoImage

--- src/components/partials/Cards.jsx
+++ src/components/partials/Cards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import noImage from "/noImage.jpg";
 const Cards = ({ data, title }) => {
   return (
     <div className="flex flex-wrap w-[full] mt-[2%] h-full  px-[4%] bg-[#1F1E24]  ">
@@ -12,9 +12,10 @@ const Cards = ({ data, title }) => {
         >
           <img
             className="shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)]  h-[40vh]object-cover"
-            src={`https://image.tmdb.org/t/p/original/${
+            src={
+              c.poster_path || c.backdrop_path || c.profile_path ?`https://image.tmdb.org/t/p/original/${
               c.poster_path || c.backdrop_path || c.profile_path
-            }`}
+            }`:noImage}
             alt=""
           />
           <h1 className="text-xl text-zinc-300 mt-3 font-semibold">
